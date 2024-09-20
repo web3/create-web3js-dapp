efb93c543e7251669d24549de01faccf0a4956b9
Dan Forbes <dan.forbes@chainsafe.io>
2024-09-20 16:02:26 -0400

    Small Changes to web3.service.ts for web3js-angular-dapp-min

--- templates/min/web3js-angular-dapp-min/src/app/web3/web3.service.ts
+++ templates/min/web3js-angular-dapp-min/src/app/web3/web3.service.ts
@@ -1,10 +1,20 @@
 import { Injectable, type OnDestroy } from '@angular/core';
 import { type EIP1193Provider, Web3, type Web3APISpec } from 'web3';
 
+declare global {
+  interface Window {
+    ethereum?: EIP1193Provider<Web3APISpec>;
+  }
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class Web3Service implements OnDestroy {
+  private static handleChainChanged() {
+    window.location.reload();
+  }
+
   web3: Web3 = new Web3();
   provider: EIP1193Provider<Web3APISpec> | undefined;
 
@@ -16,22 +26,12 @@ export class Web3Service implements OnDestroy {
     this.provider = window.ethereum;
     this.web3.setProvider(this.provider);
 
-    this.provider.on('chainChanged', this.handleChainChanged);
+    this.provider.on('chainChanged', Web3Service.handleChainChanged);
   }
 
   ngOnDestroy(): void {
     if (this.provider) {
-      this.provider.removeListener('chainChanged', this.handleChainChanged);
+      this.provider.removeListener('chainChanged', Web3Service.handleChainChanged);
     }
-  }
-
-  private async handleChainChanged() {
-    window.location.reload();
-  }
-}
-
-declare global {
-  interface Window {
-    ethereum?: EIP1193Provider<Web3APISpec>;
   }
 }
