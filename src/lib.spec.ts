a34f9c9da00b70aea5a905c190a8c636ea7e05ec
Dan Forbes <dan.forbes@chainsafe.io>
2024-09-24 05:41:00 -0700

    Initial Commit for create-web3js-dapp (#5)

---
+++ src/lib.spec.ts
@@ -0,0 +1,31 @@
+import { strict as assert } from "assert";
+import { test } from "node:test";
+import { join } from "path";
+
+import { getSelectedOption } from "./lib";
+import type { Options, SelectedOption } from "./types";
+
+test("getSelectedOption(angular, minimal)", () => {
+  const opts: Options = { framework: "angular", template: "minimal" };
+  const result: SelectedOption = getSelectedOption(opts);
+
+  const expectedName: string = "web3js-angular-dapp-min";
+  assert.strictEqual(
+    result.projectName,
+    expectedName,
+    "unexpected project name",
+  );
+
+  const expectedLocation: string = join(
+    __dirname,
+    "..",
+    "templates",
+    "min",
+    expectedName,
+  );
+  assert.strictEqual(
+    result.projectLocation,
+    expectedLocation,
+    "unexpected project location",
+  );
+});
