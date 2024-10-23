e1d08c928223e0faa3f167de075351b3b498dcd2
Dan Forbes <dan.forbes@chainsafe.io>
2024-10-23 12:50:16 -0700

    Various Small Fixes (#15)
    Closes #12
    Closes #14

--- templates/demo/web3js-react-dapp-demo/src/App.tsx
+++ templates/demo/web3js-react-dapp-demo/src/App.tsx
@@ -42,7 +42,7 @@ function App() {
 
   return (
     <main>
-      <h1>Web3.js + React Minimal dApp Template</h1>
+      <h1>Web3.js + React Demonstration dApp</h1>
       <div>
         This is a sample project that demonstrates using{" "}
         <a href="https://web3js.org/">Web3.js</a> with the{" "}
